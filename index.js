1c203bd1f6aee0e423bf7be2bb286f86b9106658
Daisy Simiyu <d.daisysimiyu@gmail.com>
2022-06-20 05:13:30 +0300

    commit tree f4b5ccf7e3a9f544fcafc40807d4b029ab2198d6

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const arrayOfNumbers = {};
+
+  for (const number of array) {
+    // n steps
+    const complement = target - number;
+    if (complement in arrayOfNumbers) return true;
+    arrayOfNumbers[number] = true;
+  }
+
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  
+  time: O(n^2)
+  space: O(n)
+
+ 
 */
 
 /* 
+
   Add your pseudocode here
+  
+     iterate through each number in array
+    
 */
 
 /*
